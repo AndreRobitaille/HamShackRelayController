4a5153e34559cd8f63c373290b8c616385d5b7b1
Andre Robitaille <7423320+AndreRobitaille@users.noreply.github.com>
2022-01-24 10:53:30 -0600

    Moved readme to file. Removed blank line hashes.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # HamShackRelayController
+
+Name
+==============
+Ham Shack Relay Controller
+
+Description
+==============
+Scott's GTK Python program for turning on/off relays for his ham radio shack using a Raspberry Pi and relay hats.
+
+Installation
+==============
+
+Linux Packages and Apps
+--------------
+Requires the following:
+- Thonny IDE
+- Python 3 for base language
+- GTK 3 for GUI
+- GI
+- Time for timer functions
+- Piplates for relays
+
+Making the app autostart
+--------------
+    sudo nano /etc/xdg/lxsession/LXDE-pi/autostart
+    add @python3 /home/pi/Documents/PythonApps/Source/Shack.py
+
+That's it!
--- Shack.py
+++ Shack.py
@@ -5,24 +5,12 @@ import os as SYSTEM
 import time
 import piplates.RELAYplate as RELAY
 import pygame
-# How to setup autostart
-# sudo nano /etc/xdg/lxsession/LXDE-pi/autostart
-# add @python3 /home/pi/Documents/PythonApps/Source/Shack.py
-#that's it!
-# -*- coding: utf-8 -*-
-# Scottś GTK - Python program for turning on/off relays for shack.
-# This program requires the following:
-# Thonny IDE
-# Python 3 for base language
-# GTK 3 for GUI
-# GI
-# Time for timer functions
-# Piplates for relays
+
 # Pygame for audio
-#
 pygame.mixer.init()
 pygame.mixer.music.load("/home/pi/Documents/PythonApps/Sounds/click.wav")
 RELAY.getID(0)
+
 # Relay Key:
 #
 # 0,1 Left Speaker Mute
@@ -39,6 +27,7 @@ RELAY.getID(0)
 # 1,5 Audio System
 # 1,6 unused
 # 1,7 unused
+
 # Initalize 12VDC relays off
 #RELAY.relayOFF(0, 1)
 #RELAY.relayOFF(0, 2)
@@ -47,6 +36,7 @@ RELAY.getID(0)
 #RELAY.relayOFF(0, 5)
 #RELAY.relayOFF(0, 6)
 #RELAY.relayOFF(0, 7)
+
 # Initialize 120VAC relays off
 #RELAY.relayOFF(1, 1)
 #RELAY.relayOFF(1, 2)
@@ -55,98 +45,99 @@ RELAY.getID(0)
 #RELAY.relayOFF(1, 5)
 #RELAY.relayOFF(1, 6)
 #RELAY.relayOFF(1, 7)
+
 # Turn on lights
 RELAY.relayON(1, 3)
-#
-#
+
+
 class Relay_Control_Window(Gtk.Window):
-#
+
     def __init__(self):
         Gtk.Window.__init__(self, title="KC9WPS Shack Device Controller")
         grid = Gtk.Grid()
         self.add(grid)
         self.fullscreen()
-#
+
         button100 = Gtk.ToggleButton(label="Normal\nShutdown")
         grid.add(button100)
         button100.connect("toggled", self.on_button_toggled100,"100")
-#
+
         button110 = Gtk.ToggleButton(label="Auto\nPower\nUp")
         grid.attach_next_to(button110, button100, Gtk.PositionType.RIGHT, 1, 1)
         button110.connect("toggled", self.on_button_toggled110, "110")
-#
+
         button120 = Gtk.ToggleButton(label="Full\nShut \nDown")
         grid.attach_next_to(button120, button110, Gtk.PositionType.RIGHT, 1, 1)
         button120.connect("toggled", self.on_button_toggled120, "120")
-#
+
         button130 = Gtk.ToggleButton(label="Mute\nAudio")
         grid.attach_next_to(button130, button120, Gtk.PositionType.RIGHT, 1, 1)
         button130.connect("toggled",self.on_button_toggled130,"130")
-#
+
         button140 = Gtk.ToggleButton(label="Lights\nOn")
         grid.attach_next_to(button140, button130, Gtk.PositionType.RIGHT, 1, 1)
         button140.connect("toggled", self.on_button_toggled140, "140")
-#
+
         button150 = Gtk.ToggleButton(label="Audio\nSystem")
         grid.attach_next_to(button150, button140, Gtk.PositionType.RIGHT, 1, 1)
         button150.connect("toggled", self.on_button_toggled150, "150")
-#
+
         button3 = Gtk.ToggleButton(label="12VDC\nPower")
         grid.attach_next_to(button3, button100, Gtk.PositionType.BOTTOM, 1, 1)
         button3.connect("toggled", self.on_button_toggled3, "3")
-#
+
         button4 = Gtk.ToggleButton(label="Dual\nMonitor")
         grid.attach_next_to(button4, button3, Gtk.PositionType.RIGHT, 1, 1)
         button4.connect("toggled", self.on_button_toggled4, "4")
-#
+
         button5 = Gtk.ToggleButton(label="Aux\nDevices")
         grid.attach_next_to(button5, button4, Gtk.PositionType.RIGHT, 1, 1)
         button5.connect("toggled", self.on_button_toggled5, "5")
-#
+
         button6 = Gtk.ToggleButton(label="Thermal\nControl")
         grid.attach_next_to(button6, button5, Gtk.PositionType.RIGHT, 1, 1)
         button6.connect("toggled", self.on_button_toggled6, "6")
-#
+
         button7 = Gtk.ToggleButton(label="HF-1\nYaesu\n101MP")
         grid.attach_next_to(button7, button6, Gtk.PositionType.RIGHT, 1, 1)
         button7.connect("toggled", self.on_button_toggled7, "7")
-#
+
         button8 = Gtk.ToggleButton(label="HF-2\nIcom\n7300")
         grid.attach_next_to(button8, button7, Gtk.PositionType.RIGHT, 1, 1)
         button8.connect("toggled", self.on_button_toggled8, "8")
-#
+
         button9 = Gtk.ToggleButton(label="VHF-1\nYaesu\n7900")
         grid.attach_next_to(button9, button8, Gtk.PositionType.RIGHT, 1, 1)
         button9.connect("toggled", self.on_button_toggled9, "9")
-#
+
         button10 = Gtk.ToggleButton(label="Amplifier\nAmeritron\nALS-1300")
         grid.attach_next_to(button10, button3, Gtk.PositionType.BOTTOM, 1, 1)
         button10.connect("toggled", self.on_button_toggled10, "10")
-#
+
         button11 = Gtk.ToggleButton(label="Unused_1")
         grid.attach_next_to(button11, button10, Gtk.PositionType.RIGHT, 1, 1)
         button11.connect("toggled", self.on_button_toggled11, "11")
-#
+
         button12 = Gtk.ToggleButton(label="Unused_2")
         grid.attach_next_to(button12, button11, Gtk.PositionType.RIGHT, 1, 1)
         button12.connect("toggled", self.on_button_toggled12, "12")
-#
+
         button13 = Gtk.ToggleButton(label="Unused_3")
         grid.attach_next_to(button13, button12, Gtk.PositionType.RIGHT, 1, 1)
         button13.connect("toggled", self.on_button_toggled13, "13")
-#
+
         button14 = Gtk.ToggleButton(label="Unused_4")
         grid.attach_next_to(button14, button13, Gtk.PositionType.RIGHT, 1, 1)
         button14.connect("toggled", self.on_button_toggled14, "14")
-#
+
         button15 = Gtk.ToggleButton(label="Full\nSystem\nShutdown")
         grid.attach_next_to(button15, button14, Gtk.PositionType.RIGHT, 1, 1)
         button15.connect("toggled", self.on_button_toggled15, "15")
-#
+
         button16 = Gtk.ToggleButton(label="Exit to OS")
         grid.attach_next_to(button16, button15, Gtk.PositionType.RIGHT, 1, 1)
         button16.connect("toggled", self.on_button_toggled16, "16")
-#
+
 # Normal Shutdown - turn off all power relays
     def on_button_toggled100(self, button100, name):
         while pygame.mixer.music.get_busy() == True:
@@ -533,8 +524,8 @@ class Relay_Control_Window(Gtk.Window):
                 continue
         print("exit here to OS")
         quit()
-#
-#
+
+
 win = Relay_Control_Window()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
